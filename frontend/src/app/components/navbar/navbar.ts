1a89ba13e4b7b306283e1b0dc31a82e6e9e001cb
Amir <age81737@lasallebajio.edu.mx>
2026-04-27 13:15:55 -0600

    Menú hamburguesa funcional y mejoras de diseño móvil

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, HostListener } from '@angular/core';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -12,4 +13,33 @@ import { AuthService } from '../../services/auth.service';
 })
 export class Navbar {
   authService = inject(AuthService);
+  private router = inject(Router);
+
+  menuOpen = false;
+
+  constructor() {
+    // Cierra el menú al navegar
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.menuOpen = false;
+    });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  // Cierra si se hace clic fuera del navbar
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.navbar-island')) {
+      this.menuOpen = false;
+    }
+  }
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 export interface User {
   id: number;
